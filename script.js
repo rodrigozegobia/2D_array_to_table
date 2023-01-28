780dd89e11ef5619cea9a16e3410222e5f751d5f
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-01-28 16:15:02 -0300

    Update script.js
    Adds function for removing falsey values  from array first position until it finds a non-falsey value. Also makes buildTable a function.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 /*
 var table = document.createElement("table");
+shift() removes first element of array
 */
 
 var arr = prompt("Enter an array, separated by commas. Eg. Roger, John").split(",");
 var table = document.getElementById("table");
 var valid = arr.filter(val => typeof val === 'string' || !val);
 
-for (var i = 0; i < valid.length; i++) {
-    var row = table.insertRow();
-    var cell = row.insertCell();
-    cell.innerHTML = valid[i];
+function removeFalseyAtBeginning(arr){
+    while (!arr[0]) {
+        arr.shift()
+    }
+    console.log(arr)
 }
+
+function buildTable(){
+    for (var i = 0; i < valid.length; i++) {
+        var row = table.insertRow();
+        var cell = row.insertCell();
+        cell.innerHTML = valid[i];
+    }
+}
+
+removeFalseyAtBeginning(valid)
+buildTable()
