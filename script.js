a922c5d268e10c03b06344232a599a5a2378dff8
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-01-28 23:29:52 -0300

    Update script.js
    I'm unable to make progress beyond this point. I'm abandoning this project, for now. :)

--- script.js
+++ script.js
@@ -30,53 +30,61 @@ function findLongestSubArrayLength(arr) {
 }
 
 function createTable(numCols, data) {
-  // Get the table element by ID
-  var table = document.getElementById("table");
-
-  // Create thead element
-  var thead = document.createElement("thead");
-
-  // Create tr element
-  var row = document.createElement("tr");
-
-  // Create th elements and add them to the tr element
-  for (var i = 1; i <= numCols; i++) {
-    var col = document.createElement("th");
-    col.innerHTML = i;
-    row.appendChild(col);
-  }
-
-  // Add the row to the thead
-  thead.appendChild(row);
-
-  // Add thead to the table
-  table.appendChild(thead);
-
-  // Create rows based on the data array
-  var previousRowSpan = 1;
-  for (var i = 0; i < data.length; i++) {
-    // Create a new row
+    // Get the table element by ID
+    var table = document.getElementById("table");
+  
+    // Create thead element
+    var thead = document.createElement("thead");
+  
+    // Create tr element
     var row = document.createElement("tr");
-    // Create cells for each sub-array
-    for (var j = 0; j < data[i].length; j++) {
-      var cell = document.createElement("td");
-      // Add colspan attribute to the cell
-      var colspan = numCols - data[i].length + 1;
-      if (j === data[i].length - 1) {
-        cell.setAttribute("colspan", colspan);
+  
+    // Create th elements and add them to the tr element
+    for (var i = 1; i <= numCols; i++) {
+      var col = document.createElement("th");
+      col.innerHTML = i;
+      row.appendChild(col);
+    }
+  
+    // Add the row to the thead
+    thead.appendChild(row);
+  
+    // Add thead to the table
+    table.appendChild(thead);
+  
+    // Create rows based on the data array
+    var previousRowSpan = 1;
+    for (var i = 0; i < data.length; i++) {
+      // Create a new row
+      var row = document.createElement("tr");
+      // Create cells for each sub-array
+      var colspanCount = 0;
+      for (var j = 0; j < data[i].length; j++) {
+        if(data[i][j] !== undefined){
+          var cell = document.createElement("td");
+          cell.innerHTML = data[i][j];
+          row.appendChild(cell);
+          colspanCount +=1;
+        }
+        else{
+          colspanCount +=1;
+        }
       }
-      cell.innerHTML = data[i][j];
-      row.appendChild(cell);
+      // Add colspan to the last non-undefined <td> element
+      var lastNonUndefinedTd = row.querySelectorAll("td:last-child")[0];
+      lastNonUndefinedTd.setAttribute("colspan", numCols - colspanCount + 1);
+      if (data[i].length === 0) {
+        row.setAttribute("rowspan", previousRowSpan);
+        previousRowSpan = 1;
+      } else {
+        previousRowSpan++;
+      }
+      // Add the row to the table
+      table.appendChild(row);
     }
-    if (data[i].length === 0) {
-      row.setAttribute("rowspan", previousRowSpan);
-      previousRowSpan = 1;
-    } else {
-      previousRowSpan++;
-    }
-    // Add the row to the table
-    table.appendChild(row);
   }
-}
 
-createTable(findLongestSubArrayLength(removeEmptySubArrays(arr)), removeEmptySubArrays(arr));
+createTable(
+  findLongestSubArrayLength(removeEmptySubArrays(arr)),
+  removeEmptySubArrays(arr)
+);
