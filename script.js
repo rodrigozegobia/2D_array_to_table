d862e14aa4fe45c0f1ef24cc3266999fc3724427
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-01-28 21:20:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,55 @@ let arr = [
     ['foo','bar'],
     ['foo'],
     [],
-    ['foo',,,,'bar',1]
+    ['foo',,,,'bar']
 ];
 
-function filterSubArrays(arr) {
-    const filteredArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      const subArr = arr[i].filter(val => typeof val === 'string' || !val);
-      filteredArr.push(subArr);
+/*function removeEmptySubArrays(arr) {
+    return arr.filter(subArr => subArr.some(el => el !== ''));
+}*/
+
+function removeEmptySubArrays(arr) {
+    while (arr.length > 0 && arr[0].every(el => (el === '' || el === undefined || el.length === 0))) {
+        arr.shift();
     }
-    return filteredArr;
+    if (arr.length === 0) {
+        return []
+    }
+    return arr.filter(subArr => subArr.some(el => el !== '' && el !== undefined && el.length !== 0));
+}
+
+function findLongestSubArrayLength(arr) {
+    let longestLength = 0;
+    arr.forEach(subArr => {
+        if (subArr.length > longestLength) {
+            longestLength = subArr.length;
+        }
+    });
+    return longestLength;
+}
+
+function createTable(numCols) {
+    // Get the table element by ID
+    var table = document.getElementById("table");
+    
+    // Create thead element
+    var thead = document.createElement("thead");
+    
+    // Create tr element
+    var row = document.createElement("tr");
+    
+    // Create th elements and add them to the tr element
+    for (var i = 1; i <= numCols; i++) {
+      var col = document.createElement("th");
+      col.innerHTML = i;
+      row.appendChild(col);
+    }
+    
+    // Add the row to the thead
+    thead.appendChild(row);
+    
+    // Add thead to the table
+    table.appendChild(thead);
   }
 
-function removeFalsyAtBeginning(array) { 
-    for (let i = 0; i < array.length; i++) {
-        while (!array[i][0]) {
-            array[i][0].shift();
-        }
-    }
-}
-
-/*
-function getLongestArray(arr){
-    let longest = arr.reduce((a, b) => {
-      return a.length > b.length ? a : b;
-    });
-    console.log(longest.length);
-}
-console.log(arr)*/
-removeFalsyAtBeginning(arr)
-console.log(arr)
+createTable(findLongestSubArrayLength(removeEmptySubArrays(arr)))
